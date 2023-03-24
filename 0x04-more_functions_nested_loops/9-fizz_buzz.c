7334994942185d3bacfe0730cf54ed2cc6eaec4d
Ma95hdi <kamalmahdi040@gmail.com>
2023-03-24 09:04:59 -0700

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,8 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-	i	if (i % 3 == 0 && i % 5 != 0)
+		if (i % 3 == 0 && i % 5 != 0)
+
 		{
 			printf(" Fizz");
 		} else if (i % 5 == 0 && i % 3 != 0)
