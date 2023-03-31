af4ba8731b404568d28380aab18923f0aecb4f19
Ma95hdi <kamalmahdi040@gmail.com>
2023-03-31 04:31:16 -0700

    concatenate twos strings strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,13 +8,15 @@
   */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0;
-	int j = 0;
+	int i;
+	int j;
 
+	i = 0;
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
+	j = 0;
 	while (j < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
